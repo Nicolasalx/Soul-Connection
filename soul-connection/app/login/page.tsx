331a196f9504c8af9c86bce6a2395b4696540f73
Alexandre VIGOUREUX <alexandre.vigoureux@epitech.eu>
2024-09-02 17:31:35 +0200

    feat(authentication): basic login page, not yet working with the API

---
+++ soul-connection/app/login/page.tsx
@@ -0,0 +1,41 @@
+'use client'
+
+import { redirect } from 'next/navigation'
+import { FormEvent } from 'react'
+
+export default function Login() {
+  async function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    const formData = new FormData(event.currentTarget)
+    const email = formData.get('email')
+    const password = formData.get('password')
+
+    try {
+        const response = await fetch('https://soul-connection.fr/api/employees/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email, password }),
+        })
+
+        if (response.ok) {
+            redirect('/dashboard')
+        } else {
+            alert(response.text)
+        }
+    } catch(err) {
+        alert(err)
+    }
+  }
+
+  return (
+    <div className='flex justify-center m-4'>
+        <form className='bg-[#b99dd7] rounded flex flex-col items-center p-4 shadow-lg' onSubmit={handleSubmit}>
+            <h1 className='m-2'>Login</h1>
+            <input className='bg-gray-200 p-2 shadow-inner rounded m-2' type="email" name="email" aria-label="email" placeholder="Email" required />
+            <input className='bg-gray-200 p-2 shadow-inner rounded m-2' type="password" name="password" aria-label="password" placeholder="Password" required />
+            <button className='btn btn-primary m-2' type="submit">Login</button>
+        </form>
+    </div>
+  )
+}
--- soul-connection/app/page.tsx
+++ soul-connection/app/page.tsx
@@ -1,9 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return (
-    <div>
-      <p>
-        Nostrud non qui id consequat occaecat fugiat commodo ut occaecat esse deserunt sit sit. Est pariatur veniam proident aliquip voluptate do. Commodo commodo proident laboris ut magna est sunt consectetur ut dolor elit qui. Qui officia ad commodo anim aliqua velit dolore dolor anim anim labore incididunt aliqua laborum. Id mollit ea eiusmod ex fugiat occaecat reprehenderit pariatur ullamco eiusmod. Velit incididunt minim qui eu ullamco duis nulla esse. Non occaecat commodo nisi ut.
-      </p>
-    </div>
-  );
+  redirect('/login');
 }
