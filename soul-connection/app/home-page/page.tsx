f9a3ca6ea5c8c6d010dfb18b2daacb0c7ed10630
Johana <johana.gaba@epitech.eu>
2024-09-02 23:40:10 +0200

    feat: (homepage) squared infos added just to see something abt the page

--- soul-connection/app/home-page/page.tsx
+++ soul-connection/app/home-page/page.tsx
@@ -1,19 +1,27 @@
 export default function HomeDashboard() {
   return (
-    <div className="p-12">
-      <div className="bg-white border border-gray-300 rounded p-72">
-        <div className="text-center mb-10">
-          <h1 className="text font-bold">Welcome to your dashboard</h1>
+    <div className="p-6">
+      <div className="bg-white border border-gray-300 p-6 rounded">
+        <h1 className="text font-bold text-gray-900 text-center mb-4">
+          Welcome to your dashboard
+        </h1>
+        <p className="text-gray-700 mb-4">Here you will find for now:</p>
+        <div className="space-y-4">
+          <div className="bg-blue-200 p-4 border border-gray-300 rounded">
+            <h2 className="text-md font-semibold">Coaches Count</h2>
+            <p>The current total number of coaches.</p>
+          </div>
+          <div className="bg-orange-200 p-4 border border-gray-300 rounded">
+            <h2 className="text-md font-semibold">Clients Count</h2>
+            <p>The current total number of clients.</p>
+          </div>
+          <div className="bg-pink-200 p-4 border border-gray-300 rounded">
+            <h2 className="text-md font-semibold">Next Meeting</h2>
+            <p>Your next incoming meeting details.</p>
+          </div>
         </div>
-        <p className="text-center mb-4">
-          Here you will find for now:
-          <ul className="list-disc list-inside ml-4 mt-2">
-            <li>The current total number of coaches on Soul Connection.</li>
-            <li>The current total number of clients on Soul Connection.</li>
-            <li>Your next incoming meeting.</li>
-          </ul>
-        </p>
       </div>
     </div>
   );
 }
+
