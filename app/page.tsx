f6126a382b8be526891607ca0b60e97f05b4f546
Alexandre VIGOUREUX <alexandre.vigoureux@epitech.eu>
2024-09-03 16:01:01 +0200

    refactor(database): updated code to NextJS 13+

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
-<<<<<<< HEAD
-export default function Home() {
-    return (<></>)
-=======
 import { redirect } from 'next/navigation';
 
 export default async function Home()
 {
-  const response = await fetch('http://localhost:3000/api/employees', { cache: 'no-store' });
+  const response = await fetch('http://localhost:3000/api/employees/index', {
+    method: 'GET',
+    cache: 'no-store'
+  });
 
   if (!response.ok) {
     console.log('db response nest pas OKKKKKKKKKKK ??!!');
@@ -19,5 +18,4 @@ export default async function Home()
   console.log(employees);
 
   redirect('/login');
->>>>>>> 21ec83dfe494e1b08742a9abe8ae6e99b1f43355
 }
