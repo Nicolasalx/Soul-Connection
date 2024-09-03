481b1eabd0462f04d5d73581eccd1128103c0dfc
Alexandre VIGOUREUX <alexandre.vigoureux@epitech.eu>
2024-09-03 14:57:20 +0200

    feat(login): middleware meant to check is user is logged in already, if not, the user is redirected to the login page

---
+++ middleware.ts
@@ -0,0 +1,21 @@
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+
+const publicRoutes = ['/login', '/']
+
+export default async function middleware(req: NextRequest) {
+    const isPublicRoute = publicRoutes.includes(req.nextUrl.pathname)
+    const token = cookies().get('token')?.value
+    console.log(`${req.url} -> ${req.nextUrl}`)
+    if (!isPublicRoute && !token) {
+        return NextResponse.redirect(new URL('/login', req.nextUrl))
+    }
+    if (isPublicRoute && token) {
+        return NextResponse.redirect(new URL('/dashboard', req.nextUrl))
+    }
+    return NextResponse.next()
+}
+
+export const config = {
+    matcher: '/((?!api|_next/static|_next/image|.*\\.png$).*)'
+}
