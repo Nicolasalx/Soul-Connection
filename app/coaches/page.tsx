024c6a47e3b3e2fff38905a2515fd99ee94daeb8
Nicolas Alexandre <nicolas1.alexandre@epitech.eu>
2024-09-05 10:49:27 +0200

    mod: separate in two different components the coaches page to have a cleaner code in the front end of the website

--- app/coaches/page.tsx
+++ app/coaches/page.tsx
@@ -1,44 +1,9 @@
 'use client';
 
-import type { FormProps } from 'antd';
-import { Button, Form, Input, Modal } from 'antd';
+import { Button, Modal, Select } from 'antd';
 import React, { useState } from 'react';
-import { createEmployee } from '../lib/dbhelper/employees';
-import Employees from "@/app/back/models/employees";
-
-/*     COACH CREATION     */
-
-type FieldType = {
-  email: string;
-  name: string;
-  surname: string;
-  birthDate: string;
-  gender: string;
-  work: string;
-};
-
-const onFinish: FormProps<FieldType>['onFinish'] = (values) => {
-  const uuid = Math.floor(Math.random() * 10 ** 15);
-
-  const employee = new Employees(
-    uuid,
-    values.email,
-    values.name,
-    values.surname,
-    values.birthDate,
-    values.gender,
-    values.work
-  );
-
-  createEmployee(employee);
-
-  console.log(employee);
-  console.log('Success:', values);
-};
-
-const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (errorInfo) => {
-  console.log('Failed:', errorInfo);
-};
+import EmployeeForm from './employeeForm';
+import type { SelectProps } from 'antd';
 
 /*     TABLE COACHES      */
 
@@ -46,7 +11,6 @@ interface CoachesData {
   key: React.Key;
   name: string;
   birthDate: string;
-  // customers: string[]; // Modify after
   lastConnection: string;
 }
 
@@ -55,34 +19,44 @@ const dataCoaches: CoachesData[] = [
     key: 1,
     name: 'Date',
     birthDate: 'date',
-    // customers: ['John', 'Max'],
     lastConnection: '18-07-2024'
   },
   {
     key: 2,
     name: 'Date',
     birthDate: 'date',
-    // customers: ['John', 'Max'],
     lastConnection: '18-07-2024'
   },
   {
     key: 3,
     name: 'Date',
     birthDate: 'date',
-    // customers: ['John', 'Max'],
     lastConnection: '18-07-2024'
   },
   {
     key: 4,
     name: 'Date',
     birthDate: 'date',
-    // customers: ['John', 'Max'],
     lastConnection: '18-07-2024'
   },
 ];
 
-function Coaches() {
+/*      SELECTION OF CUSTOMERS     */
 
+const options: SelectProps['options'] = [];
+
+for (let i = 10; i < 36; i++) {
+  options.push({
+    label: i.toString(36) + i,
+    value: i.toString(36) + i,
+  });
+}
+
+const handleChange = (value: string[]) => {
+  console.log(`selected ${value}`);
+};
+
+function Coaches() {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const showModal = () => {
@@ -99,79 +73,26 @@ function Coaches() {
 
   return (
     <>
-      <Button type="primary" onClick={showModal} style={{marginLeft: 200, marginTop: 200}}>
-        Create New Employee
-      </Button>
-      <Modal title="Employee Creation" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-        <Form
-          name="basic"
-          labelCol={{ span: 8 }}
-          wrapperCol={{ span: 16 }}
-          style={{ maxWidth: 600 }}
-          initialValues={{ remember: true }}
-          onFinish={onFinish}
-          onFinishFailed={onFinishFailed}
-          autoComplete="off"
-        >
+      <div style={{ marginLeft: 300, marginTop: 500 }}>
+        <Select
+          mode="multiple"
+          allowClear
+          style={{ width: '100%' }}
+          placeholder="Please select"
+          defaultValue={['a10', 'c12']}
+          onChange={handleChange}
+          options={options}
+        />
 
-        <Form.Item<FieldType>
-          label="E-mail"
-          name="email"
-          rules={[{ required: true, message: 'Please input your e-mail' }]}
-        >
-          <Input />
-        </Form.Item>
-
-        <Form.Item<FieldType>
-          label="Name"
-          name="name"
-          rules={[{ required: true, message: 'Please input your name' }]}
-        >
-          <Input />
-        </Form.Item>
-
-        <Form.Item<FieldType>
-          label="Surname"
-          name="surname"
-          rules={[{ required: true, message: 'Please input your surname' }]}
-        >
-          <Input />
-        </Form.Item>
-
-        <Form.Item<FieldType>
-          label="Birth Date"
-          name="birthDate"
-          rules={[{ required: true, message: 'Please input your birth date' }]}
-        >
-          <Input />
-        </Form.Item>
-
-        <Form.Item<FieldType>
-          label="Gender"
-          name="gender"
-          rules={[{ required: true, message: 'Please input your gender' }]}
-        >
-          <Input />
-        </Form.Item>
-
-        <Form.Item<FieldType>
-          label="Work"
-          name="work"
-          rules={[{ required: true, message: 'Please input your work' }]}
-        >
-          <Input />
-        </Form.Item>
-
-        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-        <Button type="primary" htmlType="submit">
-          Submit
+        <Button type="primary" onClick={showModal}>
+          Create New Employee
         </Button>
-      </Form.Item>
-    </Form>
-
-      </Modal>
+        <Modal title="Employee Creation" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+          <EmployeeForm />
+        </Modal>
+      </div>
     </>
   );
 }
 
-export default Coaches
+export default Coaches;
