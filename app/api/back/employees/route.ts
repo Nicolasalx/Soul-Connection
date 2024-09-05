2bfee2d8533e10eca775b10ad877afefc7f91378
thibaudcathala <thibaud.cathala@epitech.eu>
2024-09-05 19:50:11 +0200

    mov: utf encoder in the lib folder

--- app/api/back/employees/route.ts
+++ app/api/back/employees/route.ts
@@ -1,60 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { connectToDatabase } from '@/app/back/services/database.service';
 import { ObjectId } from 'mongodb';
-
-export function toUTF8Array(str: string)
-{
-  var utf8 = [];
-  for (var i=0; i < str.length; i++) {
-      var charcode = str.charCodeAt(i);
-      if (charcode < 0x80) utf8.push(charcode);
-      else if (charcode < 0x800) {
-          utf8.push(0xc0 | (charcode >> 6),
-                    0x80 | (charcode & 0x3f));
-      }
-      else if (charcode < 0xd800 || charcode >= 0xe000) {
-          utf8.push(0xe0 | (charcode >> 12),
-                    0x80 | ((charcode>>6) & 0x3f),
-                    0x80 | (charcode & 0x3f));
-      }
-      else {
-          i++;
-          charcode = ((charcode&0x3ff)<<10)|(str.charCodeAt(i)&0x3ff)
-          utf8.push(0xf0 | (charcode >>18),
-                    0x80 | ((charcode>>12) & 0x3f),
-                    0x80 | ((charcode>>6) & 0x3f),
-                    0x80 | (charcode & 0x3f));
-      }
-  }
-  return utf8;
-}
-
-export function fromUTF8Array(data: number[])
-{
-  var str = '',
-      i;
-
-  for (i = 0; i < data.length; i++) {
-      var value = data[i];
-
-      if (value < 0x80) {
-          str += String.fromCharCode(value);
-      } else if (value > 0xBF && value < 0xE0) {
-          str += String.fromCharCode((value & 0x1F) << 6 | data[i + 1] & 0x3F);
-          i += 1;
-      } else if (value > 0xDF && value < 0xF0) {
-          str += String.fromCharCode((value & 0x0F) << 12 | (data[i + 1] & 0x3F) << 6 | data[i + 2] & 0x3F);
-          i += 2;
-      } else {
-          var charCode = ((value & 0x07) << 18 | (data[i + 1] & 0x3F) << 12 | (data[i + 2] & 0x3F) << 6 | data[i + 3] & 0x3F) - 0x010000;
-
-          str += String.fromCharCode(charCode >> 10 | 0xD800, charCode & 0x03FF | 0xDC00);
-          i += 3;
-      }
-  }
-
-  return str;
-}
+import { fromUTF8Array, toUTF8Array } from '@/app/lib/dbhelper/utf_encoder';
 
 export async function GET(request: NextRequest)
 {
