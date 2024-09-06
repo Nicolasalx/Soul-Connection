7c9e9956f57e0ed2e8be6419b2158b299dfd0088
Nicolas Alexandre <nicolas1.alexandre@epitech.eu>
2024-09-06 16:50:50 +0200

    mod: remove unused comment in the clothes image page in the back part of the website

--- app/lib/dbhelper/clothes_image.ts
+++ app/lib/dbhelper/clothes_image.ts
@@ -4,7 +4,7 @@ import { sc_db_api } from "./db_api_instance";
 export async function getClothesImage(id: string): Promise<Blob | null>
 {
   try {
-    const response = await sc_db_api.get(`/api/back/clothes_image?id=${id}`);
+    const response = await sc_db_api.get(`clothes_image?id=${id}`);
 
     const imageBlob = await response.data.blob();
     return imageBlob;
@@ -19,7 +19,6 @@ export async function createClothesImage(id: string, imageBlob: Blob): Promise<v
         const reader = new FileReader();
         reader.readAsDataURL(imageBlob);
 
-      // Use a promise to wait until the conversion is complete
       const base64ImagePromise = new Promise<string>((resolve, reject) => {
         reader.onloadend = () => resolve(reader.result as string);
         reader.onerror = reject;
@@ -27,7 +26,6 @@ export async function createClothesImage(id: string, imageBlob: Blob): Promise<v
 
       const base64Image = await base64ImagePromise;
 
-      // Send the base64 image to your API
       const response = await fetch('/api/back/clothes_image', {
         method: 'POST',
         headers: {
@@ -35,7 +33,7 @@ export async function createClothesImage(id: string, imageBlob: Blob): Promise<v
         },
         body: JSON.stringify({
           id: id,
-          image: base64Image, // Base64 encoded image
+          image: base64Image,
         }),
       });
   
