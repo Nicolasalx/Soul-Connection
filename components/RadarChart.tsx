c29afe599b9a5161bf12afdc3d53fd8c76df46d7
Nicolas Alexandre <nicolas1.alexandre@epitech.eu>
2024-09-08 17:01:26 +0200

    add: new component to make radar chart has been added, it's used in the statistics nowadays

---
+++ components/RadarChart.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Radar, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis, Tooltip, Legend } from 'recharts';
+
+type RadarChartProps = {
+  data: { name: string; value: number }[];
+  title: string;
+};
+
+const CustomRadarChart: React.FC<RadarChartProps> = ({ data, title }) => {
+  return (
+    <div className="bg-white border border-gray-300 p-6 rounded-lg">
+      <h2 className="font-bold text-gray-600 mb-4 text-2xl">{title}</h2>
+      <RadarChart outerRadius={90} width={400} height={400} data={data}>
+        <PolarGrid />
+        <PolarAngleAxis dataKey="name" />
+        <PolarRadiusAxis />
+        <Radar name={title} dataKey="value" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
+        <Tooltip />
+        <Legend />
+      </RadarChart>
+    </div>
+  );
+};
+
+export default CustomRadarChart;
