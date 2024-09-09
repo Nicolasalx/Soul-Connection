c232ef6ef0cf0ad24bb1adf37ce4a9fa33b79252
Alexandre VIGOUREUX <alexandre.vigoureux@epitech.eu>
2024-09-09 16:47:44 +0200

    style(home): removed useless background since layout.tsx is setting it already

--- app/(pages)/home/page.tsx
+++ app/(pages)/home/page.tsx
@@ -39,53 +39,51 @@ function HomeDashboard()
   });
 
   return (
-    <div className="flex flex-col h-screen w-screen p-6">
-      <div className="bg-white border border-gray-300 p-12 rounded-lg flex-1">
-        <h1 className="font-bold text-gray-600 mb-10 mt-10 text-center text-5xl md:text-6xl">
-          SOUL CONNECTION
-        </h1>
-        <h2 className="text-gray-400 mb-5 text-center text-xl" style={{ fontSize: "2rem" }}>
-          Dashboard
-        </h2>
-        <Divider style={{ borderColor: '#d3d3d3' }} />
+    <>
+      <h1 className="font-bold text-gray-600 mb-10 mt-10 text-center text-5xl md:text-6xl">
+        SOUL CONNECTION
+      </h1>
+      <h2 className="text-gray-400 mb-5 text-center text-xl" style={{ fontSize: "2rem" }}>
+        Dashboard
+      </h2>
+      <Divider style={{ borderColor: '#d3d3d3' }} />
 
-        <div className="flex space-x-4 mb-6">
-          <div className="w-1/3">
-            <BarrChart
-              data={chartDataCoachNbEncounters}
-              title="Coaches total performances"
-              yAxisKey="coach"
-              barKey="value"
-            />
-          </div>
-          <div className="w-1/3">
-            <BarrChart
-              data={chartDataCoachesAverageDateRating}
-              title="Coaches Average Dates Rating"
-              yAxisKey="coach"
-              barKey="grade"
-            />
-          </div>
-          <div className="w-1/3">
-            <BarrChart
-              data={chartDataSalesEv}
-              title="Sales Revenus Evolution"
-              yAxisKey="month"
-              barKey="amount"
-            />
-          </div>
+      <div className="flex space-x-4 mb-6">
+        <div className="w-1/3">
+          <BarrChart
+            data={chartDataCoachNbEncounters}
+            title="Coaches total performances"
+            yAxisKey="coach"
+            barKey="value"
+          />
         </div>
-
-        <div className="mt-2">
-          <Table />
+        <div className="w-1/3">
+          <BarrChart
+            data={chartDataCoachesAverageDateRating}
+            title="Coaches Average Dates Rating"
+            yAxisKey="coach"
+            barKey="grade"
+          />
+        </div>
+        <div className="w-1/3">
+          <BarrChart
+            data={chartDataSalesEv}
+            title="Sales Revenus Evolution"
+            yAxisKey="month"
+            barKey="amount"
+          />
         </div>
       </div>
+
+      <div className="mt-2">
+        <Table />
+      </div>
       <div className="mt-4 text-center">
-        <p className="text-sm text-white">
+        <p className="text-sm">
           For more details, go to the <a href="/statistics" className="text-blue-500 underline">Statistics page</a>.
         </p>
       </div>
-    </div>
+    </>
   );
 }
 
