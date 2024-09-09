60fc5c0888fed60a8e0580afb635e40d5a843453
Nicolas Alexandre <nicolas1.alexandre@epitech.eu>
2024-09-09 11:58:54 +0200

    mod: form has been updated in the coaches page to create new employee, now the password is hide, the field e-mail is better and the birth date is a date picker, i have added popup too when we submit form

--- app/coaches/employeeForm.tsx
+++ app/coaches/employeeForm.tsx
@@ -1,105 +1,160 @@
 'use client';
 
 import type { FormProps } from 'antd';
-import { Button, Form, Input } from 'antd';
+import { Button, Form, Input, DatePicker, message } from 'antd';
 import React from 'react';
 import { createEmployee } from '../lib/dbhelper/employees';
 import Employees from "@/app/back/models/employees";
+import dayjs from 'dayjs';
 
 type FieldType = {
   email: string;
+  password: string;
   name: string;
   surname: string;
-  birthDate: string;
+  birthDate: string | null;
   gender: string;
   work: string;
 };
 
-const onFinish: FormProps<FieldType>['onFinish'] = (values) => {
-  const uuid = Math.floor(Math.random() * 10 ** 15);
+const EmployeeForm: React.FC = () => {
+  const [messageApi, contextHolder] = message.useMessage();
 
-  const employee = new Employees(
-    uuid,
-    values.email,
-    values.name,
-    values.surname,
-    values.birthDate,
-    values.gender,
-    values.work
+  const onFinish: FormProps<FieldType>['onFinish'] = async (values) => {
+    const key = 'updatable';
+
+    // Show loading message
+    messageApi.open({
+      key,
+      type: 'loading',
+      content: 'Submitting...',
+    });
+
+    const uuid = Math.floor(Math.random() * 10 ** 15);
+    const formattedBirthDate = values.birthDate ? dayjs(values.birthDate).format('YYYY-MM-DD') : '';
+
+    const employee = new Employees(
+      uuid,
+      values.email,
+      values.name,
+      values.password,
+      values.surname,
+      formattedBirthDate,
+      values.gender,
+      values.work
+    );
+
+    try {
+      await createEmployee(employee);
+      
+      // Update to success message
+      messageApi.open({
+        key,
+        type: 'success',
+        content: 'Employee created successfully!',
+        duration: 2,
+      });
+    } catch (error) {
+      // Update to error message if submission fails
+      messageApi.open({
+        key,
+        type: 'error',
+        content: 'Failed to create employee. Please try again.',
+        duration: 2,
+      });
+    }
+  };
+
+  const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (errorInfo) => {
+    // Display error message when validation fails
+    messageApi.open({
+      type: 'error',
+      content: 'Please fill in all required fields correctly.',
+      duration: 2,
+    });
+    console.log('Failed:', errorInfo);
+  };
+
+  return (
+    <>
+      {contextHolder}
+      <Form
+        name="basic"
+        labelCol={{ span: 5 }}
+        wrapperCol={{ span: 16 }}
+        style={{ maxWidth: 600 }}
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item<FieldType>
+          label="E-mail"
+          name="email"
+          rules={[
+            { required: true, message: 'Please input your e-mail' },
+            { type: 'email', message: 'Please enter a valid email' },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item<FieldType>
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: 'Please input your password!' }]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item<FieldType>
+          label="Name"
+          name="name"
+          rules={[{ required: true, message: 'Please input your name' }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item<FieldType>
+          label="Surname"
+          name="surname"
+          rules={[{ required: true, message: 'Please input your surname' }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Birth Date"
+          name="birthDate"
+          rules={[{ required: true, message: 'Please input your birth date' }]}
+        >
+          <DatePicker />
+        </Form.Item>
+
+        <Form.Item<FieldType>
+          label="Gender"
+          name="gender"
+          rules={[{ required: true, message: 'Please input your gender' }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item<FieldType>
+          label="Work"
+          name="work"
+          rules={[{ required: true, message: 'Please input your work' }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item wrapperCol={{ offset: 10, span: 16 }}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </>
   );
-
-  createEmployee(employee);
 };
-
-const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (errorInfo) => {
-  console.log('Failed:', errorInfo);
-};
-
-const EmployeeForm: React.FC = () => (
-  <Form
-    name="basic"
-    labelCol={{ span: 5 }}
-    wrapperCol={{ span: 16 }}
-    style={{ maxWidth: 600 }}
-    initialValues={{ remember: true }}
-    onFinish={onFinish}
-    onFinishFailed={onFinishFailed}
-    autoComplete="off"
-  >
-    <Form.Item<FieldType>
-      label="E-mail"
-      name="email"
-      rules={[{ required: true, message: 'Please input your e-mail' }]}
-    >
-      <Input />
-    </Form.Item>
-
-    <Form.Item<FieldType>
-      label="Name"
-      name="name"
-      rules={[{ required: true, message: 'Please input your name' }]}
-    >
-      <Input />
-    </Form.Item>
-
-    <Form.Item<FieldType>
-      label="Surname"
-      name="surname"
-      rules={[{ required: true, message: 'Please input your surname' }]}
-    >
-      <Input />
-    </Form.Item>
-
-    <Form.Item<FieldType>
-      label="Birth Date"
-      name="birthDate"
-      rules={[{ required: true, message: 'Please input your birth date' }]}
-    >
-      <Input />
-    </Form.Item>
-
-    <Form.Item<FieldType>
-      label="Gender"
-      name="gender"
-      rules={[{ required: true, message: 'Please input your gender' }]}
-    >
-      <Input />
-    </Form.Item>
-
-    <Form.Item<FieldType>
-      label="Work"
-      name="work"
-      rules={[{ required: true, message: 'Please input your work' }]}
-    >
-      <Input />
-    </Form.Item>
-
-    <Form.Item wrapperCol={{ offset: 10, span: 16 }}>
-      <Button type="primary" htmlType="submit">
-        Submit
-      </Button>
-    </Form.Item>
-  </Form>
-);
 
 export default EmployeeForm;
