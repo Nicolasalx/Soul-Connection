09046f49c5561a90557a8ad3ea12cd38de51aaf6
thibaudcathala <thibaud.cathala@epitech.eu>
2024-09-10 22:32:31 +0200

    fix: password encryption

--- app/api/back/employees_image/route.ts
+++ app/api/back/employees_image/route.ts
@@ -32,7 +32,7 @@ export async function POST(request: NextRequest)
 
 export async function GET(request: NextRequest)
 {
-try {
+  try {
     const db = await connectToDatabase();
     const employeesCollection = db.collection('employees_image');
 
