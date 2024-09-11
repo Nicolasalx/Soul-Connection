8bb823f3957b8b8763cdcda3ee68bf63f996390d
Nicolas Alexandre <nicolas1.alexandre@epitech.eu>
2024-09-11 19:08:11 +0200

    mod: architecture has been modified, now we have 5 pages for the customers

--- app/(pages)/customer/advices/page.tsx
+++ app/(pages)/customer/advices/page.tsx
@@ -1,3 +1,57 @@
-export default function Advices() {
-    return <div>Advices</div>
+'use client';
+
+import React from 'react';
+import { Divider, Table } from 'antd';
+import type { TableColumnsType } from 'antd';
+
+interface DataType {
+  key: React.Key;
+  name: string;
+  age: number;
+  address: string;
+}
+
+const columns: TableColumnsType<DataType> = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+  },
+];
+
+const data: DataType[] = [
+  {
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sydney No. 1 Lake Park',
+  },
+];
+
+export default function Advices()
+{
+  return (
+    <>
+      <Table columns={columns} dataSource={data} size="middle" />
+    </>
+  );
 }
