16c11c093968f51e0b7ed639e788f58c4eaf96ef
Johana <johana.gaba@epitech.eu>
2024-09-12 09:15:23 +0200

    feat: bars in navbar, need to be lowered down

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -12,6 +12,7 @@ import { isCustomer, isManager } from "@/app/lib/user";
 
 const SideBarCustomer = (handleLogout: () => Promise<void>, openDBPopup: () => void) => {
   const [hasRights, setHasRights] = useState(false);
+  const pathname = usePathname();
 
   useEffect(() => {
     isManager().then(val => setHasRights(val));
@@ -21,27 +22,27 @@ const SideBarCustomer = (handleLogout: () => Promise<void>, openDBPopup: () => v
     <div className="w-full h-24 bg-white px-4 flex items-center border-b border-color global-outfit">
       Soul Connection
       <ul className="flex flex-grow gap-x-6 text-black text-l items-center justify-center global-outfit">
-        <li>
+        <li className={pathname === "/customer/home" ? "border-b-2 border-blue-500" : ""}>
           <Link href="/customer/home">
             <p>Home</p>
           </Link>
         </li>
-        <li>
+        <li className={pathname === "/customer/chat" ? "border-b-2 border-blue-500" : ""}>
           <Link href="/customer/chat">
             <p>Chat</p>
           </Link>
         </li>
-        <li>
+        <li className={pathname === "/customer/advices" ? "border-b-2 border-blue-500" : ""}>
           <Link href="/customer/advices">
             <p>Advices</p>
           </Link>
         </li>
-        <li>
+        <li className={pathname === "/customer/notes" ? "border-b-2 border-blue-500" : ""}>
           <Link href="/customer/notes">
             <p>Notes</p>
           </Link>
         </li>
-        <li>
+        <li className={pathname === "/customer/encounters" ? "border-b-2 border-blue-500" : ""}>
           <Link href="/customer/encounters">
             <p>Encounters</p>
           </Link>
@@ -90,6 +91,7 @@ const SideBarCustomer = (handleLogout: () => Promise<void>, openDBPopup: () => v
 
 const SideBarEmployee = (handleLogout: () => Promise<void>, openDBPopup: () => void) => {
   const [hasRights, setHasRights] = useState(false);
+  const pathname = usePathname();
 
   useEffect(() => {
     isManager().then(val => setHasRights(val));
@@ -99,56 +101,56 @@ const SideBarEmployee = (handleLogout: () => Promise<void>, openDBPopup: () => v
     <div className="w-full h-24 bg-white px-4 flex items-center border-b border-color outfit-font">
       Soul Connection
       <ul className="flex flex-grow gap-x-6 text-black text-l items-center justify-center">
-        <li>
+        <li className={pathname === "/customer/home" ? "border-b-2 border-blue-500" : ""}>
           <Link href="/employee/home">
             <p>Dashboard</p>
           </Link>
         </li>
         {hasRights && (
-          <li>
+          <li className={pathname === "/customer/home" ? "border-b-2 border-blue-500" : ""}>
             <Link href="/employee/coaches">
               <p>Coaches</p>
             </Link>
           </li>
         )}
-        <li>
+        <li className={pathname === "/customer/home" ? "border-b-2 border-blue-500" : ""}>
           <Link href="/employee/customers">
             <p>Customers</p>
           </Link>
         </li>
         {hasRights && (
-          <li>
+          <li className={pathname === "/customer/home" ? "border-b-2 border-blue-500" : ""}>
             <Link href="/employee/statistics">
               <p>Statistics</p>
             </Link>
           </li>
         )}
-        <li>
+        <li className={pathname === "/customer/home" ? "border-b-2 border-blue-500" : ""}>
           <Link href="/employee/tips">
             <p>Tips</p>
           </Link>
         </li>
-        <li>
+        <li className={pathname === "/customer/home" ? "border-b-2 border-blue-500" : ""}>
           <Link href="/employee/events">
             <p>Events</p>
           </Link>
         </li>
-        <li>
+        <li className={pathname === "/customer/home" ? "border-b-2 border-blue-500" : ""}>
           <Link href="/employee/astro-compatibility">
             <p>Astrology</p>
           </Link>
         </li>
-        <li>
+        <li className={pathname === "/customer/home" ? "border-b-2 border-blue-500" : ""}>
           <Link href="/employee/clothing">
             <p>Clothing</p>
           </Link>
         </li>
-        <li>
+        <li className={pathname === "/customer/home" ? "border-b-2 border-blue-500" : ""}>
           <Link href="/employee/advices">
             <p>Advices</p>
           </Link>
         </li>
-        <li>
+        <li className={pathname === "/customer/home" ? "border-b-2 border-blue-500" : ""}>
           <Link href="/employee/notes">
             <p>Notes</p>
           </Link>
